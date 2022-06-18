66802492b58c6176d1fe453ef565515306abda1a
Артем Торшин <avtorca4@gmail.com>
2022-06-18 16:19:52 +0300

    feat!: new functional

--- Makefile
+++ Makefile
@@ -4,11 +4,15 @@ brain-even: #запуск умных игр
 	poetry run brain-even
 brain-calc: #запуск умных игр
 	poetry run brain-calc
+brain-gcd: #запуск умных игр
+	poetry run brain-gcd
 build: #упаковка пакета
 	poetry build
 publish: #публикация проекта без добавления в индекс
 	poetry publish --dry-run
 package-install: #установка пакета
-	python3 -m pip install --user dist/hexlet_code-0.3.0-py3-none-any.whl
+	python3 -m pip install --user dist/hexlet_code-0.4.0-py3-none-any.whl
 lint: #запуск линтера
 	poetry run flake8 brain_games
+inst: # обновление пакета
+	python3 -m pip install --upgrade --force-reinstall dist/hexlet_code-0.4.0-py3-none-any.whl
--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -1,5 +1,6 @@
 import prompt
 import random
+from functools import reduce
 
 
 def welcome_user():
@@ -28,6 +29,35 @@ def great_answer(num_1, num_2, operator):
         return res
 
 
+def find_common_divisor(a, b):
+    lst_a = []
+    lst_b = []
+    new_lst = []
+    d = 2
+    while d * d <= a:
+        if a % d == 0:
+            lst_a.append(d)
+            a //= d
+        else:
+            d += 1
+    if a > 1:
+        lst_a.append(a)
+    d = 2
+    while d * d <= b:
+        if b % d == 0:
+            lst_b.append(d)
+            b //= d
+        else:
+            d += 1
+    if b > 1:
+        lst_b.append(b)
+    res = lst_a if len(lst_a) < len(lst_b) else lst_b
+    for i in res:
+        if i in lst_a and i in lst_b:
+            new_lst.append(i)
+    return reduce(lambda x, y: x*y, new_lst)
+
+
 def logic_even():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
@@ -38,11 +68,11 @@ def logic_even():
         print(f'Question: {num}')
         answer = prompt.string('Your answer: ')
 
-        if num % 2 == 0 and answer == 'Yes':
+        if num % 2 == 0 and answer == 'yes':
             a = 'Correct!'
             print(a)
             count += 1
-        elif num % 2 != 0 and answer == 'No':
+        elif num % 2 != 0 and answer == 'no':
             a = 'Correct!'
             print(a)
             count += 1
@@ -80,8 +110,32 @@ def logic_calc():
             print(a)
             count += 1
         else:
-            a = f"'{answer}' is wrong answer;(.\
-Correct answer was '{great_reply}.\nLet\'s try again, {name}!'"
+            a = f"'{answer}' is wrong answer ;(.\
+Correct answer was '{great_reply}'.\nLet\'s try again, {name}!'"
+            print(a)
+            count = 3
+    victory(a, name)
+
+
+def logic_gcd():
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+
+    count = 0
+    while count != 3:
+        num_1 = random.randrange(5, 150, 5)
+        num_2 = random.randrange(5, 100, 5)
+        print(f'Question: {num_1} {num_2}')
+        answer = prompt.string('Your answer: ')
+
+        if answer == str(find_common_divisor(num_1, num_2)):
+            a = 'Correct!'
+            print(a)
+            count += 1
+        else:
+            a = f"\'{answer}\' is wrong answer ;(.\
+Correct answer was '{find_common_divisor(num_1, num_2)}'.\
+\nLet\'s try again, {name}!"
             print(a)
             count = 3
     victory(a, name)
