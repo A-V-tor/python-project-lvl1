dfc0449fcc24e891e5c0014051a26d4c1b3c7791
Артем Торшин <avtorca4@gmail.com>
2022-06-18 19:36:46 +0300

    fix:brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games.games import game
 
 
 def main():
-    print('Welcome to the Brain games!')
-    cli.welcome_user()
+    game.welcome_user()
 
 
 if __name__ == '__main__':
